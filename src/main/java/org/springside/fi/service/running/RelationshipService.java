e6797526f1cc515d4f5e48a67844dad8de7292bc
tunding <zhichaowang@126.com>
2015-06-19 18:04:13 +0800

    黑名单完成拉黑，取消拉黑。删除好友service完成

--- src/main/java/org/springside/fi/service/running/RelationshipService.java
+++ src/main/java/org/springside/fi/service/running/RelationshipService.java
@@ -21,6 +21,7 @@ import org.springside.modules.mapper.JsonMapper;
 @Transactional("transactionManager")
 public class RelationshipService extends BaseThirdService{
 	private final String RongHOSTBlacklistAdd = SystemGlobal.getConfig("RongCloudAPI")+"user/blacklist/add.json";
+	private final String RongHOSTBlacklistRemove = SystemGlobal.getConfig("RongCloudAPI")+"user/blacklist/remove.json";
 	protected JsonMapper jsonMapper = JsonMapper.nonDefaultMapper();
 	
 	@Autowired
@@ -30,9 +31,9 @@ public class RelationshipService extends BaseThirdService{
 	@Autowired
 	private RelationshipDao relationshipDao;
 	
-	public void updateRelationship(long id, String opt, String passiveAttentionUuid) {
+	public String updateRelationship(long id, String opt, String passiveAttentionUuid) {
 		String attentionUuid = getUuid(id);
-		if(opt.equals("attention")){
+		if(opt.equals("add")){
 			if(isFriend(id, passiveAttentionUuid)){
 				
 			}else if(isblack(id, passiveAttentionUuid)){
@@ -40,14 +41,24 @@ public class RelationshipService extends BaseThirdService{
 			}else{
 				attention(attentionUuid, passiveAttentionUuid);
 			}
-		}else if(opt.equals("cancel")){
-			cancelAttention(attentionUuid, passiveAttentionUuid);
+		}else if(opt.equals("remove")){
+			try{
+				if(isblack(id, passiveAttentionUuid)){
+					removeBlackList(attentionUuid, passiveAttentionUuid);
+				}
+				if(reverseIsBlack(id, passiveAttentionUuid)){
+					removeBlackList(passiveAttentionUuid, attentionUuid);
+				}
+				removeAttention(attentionUuid, passiveAttentionUuid);
+			}catch(RuntimeException e){
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	public String submitBlack(long id, String passiveAttentionUuid){
 		String attentionUuid = getUuid(id);
-		List<Relationship> relationships = relationshipDao.findRelationshipBlack(attentionUuid, passiveAttentionUuid);
+		List<Relationship> relationships = relationshipDao.findRelationshipFriend(attentionUuid, passiveAttentionUuid);
 		String res = addBlackList(attentionUuid, passiveAttentionUuid);
 		try{
 			RongCloudBlacklist blacklist = jsonMapper.fromJson(res, RongCloudBlacklist.class);
@@ -65,10 +76,14 @@ public class RelationshipService extends BaseThirdService{
 	public boolean removeBlack(long id, String passiveAttentionUuid){
 		String attentionUuid = getUuid(id);
 		List<Relationship> relationships = relationshipDao.findRelationshipBlack(attentionUuid, passiveAttentionUuid);
+		String res = removeBlackList(attentionUuid, passiveAttentionUuid);
 		try{
-			Relationship relationship = relationships.get(0);
-			relationship.setState(1);
-			relationshipDao.save(relationship);
+			RongCloudBlacklist blacklist = jsonMapper.fromJson(res, RongCloudBlacklist.class);
+			if(blacklist.getCode().equals("200")){
+				Relationship relationship = relationships.get(0);
+				relationship.setState(1);
+				relationshipDao.save(relationship);
+			}
 		}catch(RuntimeException e){
 			e.printStackTrace();
 			return false;
@@ -106,12 +121,34 @@ public class RelationshipService extends BaseThirdService{
 		}
 	}
 	
+	private void removeAttention(String attentionUuid, String passiveAttentionUuid){
+		Relationship relationship = getRelationship(attentionUuid, passiveAttentionUuid);
+		relationshipDao.delete(relationship);
+		relationship = getRelationship(passiveAttentionUuid, attentionUuid);
+		relationshipDao.delete(relationship);
+	}
+	
 	private String addBlackList(String userId, String blackUserId){
 		String reqParams = "userId="+userId+"&blackUserId="+blackUserId;
 		StringRequestEntity requestEntity = null;
 		try{
 			requestEntity = new StringRequestEntity(reqParams, "application/x-www-form-urlencoded", "UTF-8");
 			return convertResult(httpClientTemplate.executePostMethod(RongHOSTBlacklistAdd, requestEntity, null));
+		}catch(UnsupportedEncodingException e){
+			e.printStackTrace();
+			return null;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private String removeBlackList(String userId, String blackUserId){
+		String reqParams = "userId="+userId+"&blackUserId="+blackUserId;
+		StringRequestEntity requestEntity = null;
+		try{
+			requestEntity = new StringRequestEntity(reqParams, "application/x-www-form-urlencoded", "UTF-8");
+			return convertResult(httpClientTemplate.executePostMethod(RongHOSTBlacklistRemove, requestEntity, null));
 		}catch(UnsupportedEncodingException e){
 			e.printStackTrace();
 			return null;
@@ -127,11 +164,7 @@ public class RelationshipService extends BaseThirdService{
 		relationship.setPassiveAttentionUuid(passiveAttentionUuid);
 		relationshipDao.save(relationship);
 	}
-	
-	private void cancelAttention(String attentionUuid, String passiveAttentionUuid){
-		Relationship relationship = getRelationship(attentionUuid, passiveAttentionUuid);
-		relationshipDao.delete(relationship);
-	}
+
 	
 	
 	/**
