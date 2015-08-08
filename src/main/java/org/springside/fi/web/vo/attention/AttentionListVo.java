ecf8ef4e3073a9dc6bc902cf6f8c7a0a9226ed64
tunding <zhichaowang@126.com>
2015-08-08 09:40:29 +0800

    关注好友列表返回对象文件

---
+++ src/main/java/org/springside/fi/web/vo/attention/AttentionListVo.java
@@ -0,0 +1,13 @@
+package org.springside.fi.web.vo.attention;
+
+import org.springside.fi.web.vo.BaseVo;
+
+/**
+ * @author tunding:wzc@tcl.com
+ * @description 我关注的人，关注我的人列表
+ * @version 1.0
+ * @date 创建时间：2015年8月8日 上午8:51:57
+ */
+public class AttentionListVo extends BaseVo{
+	
+}
